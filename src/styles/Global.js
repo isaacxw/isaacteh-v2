2d5f97275c7836387013a58d4e7b2e7e16d7e170
Isaac <isaactxw@gmail.com>
2021-02-10 22:05:41 -0800

    Fix font size values

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -20,7 +20,7 @@ const Global = createGlobalStyle`
         --fz-xxs: 12px;
         --fz-xs: 13px;
         --fz-sm: 14px;
-        --fz-md: 14px;
+        --fz-md: 16px;
         --fz-lg: 18px;
         --fz-xl: 20px;
         --fz-xxl: 22px;
